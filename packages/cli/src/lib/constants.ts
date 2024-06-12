a9783130965f988e23a41e6a3ec262e4fc616b22
samuelhulla <oficialny.hulla@gmail.com>
2024-06-12 10:04:48 +0200

    :white_check_mark: feat: (cli) cli constants

---
+++ packages/cli/src/lib/constants.ts
@@ -0,0 +1,35 @@
+import type { Frameworks } from './types'
+
+export const intros = [
+  'What will be your next masterpiece?',
+  'Seems like a perfect day for some breathtaking UI!',
+  'At your service!',
+  'Ready to create something amazing?',
+  "Let's make your application shine!",
+  'Time to bring your ideas to life!',
+  'Creating stunning UI has never been easier!',
+  "Let's add some magic to your application!",
+  'Ready to transform your ideas into reality?',
+  'Time to create something extraordinary!',
+  'Ready to take your application to the next level?',
+  'Your creativity is the limit!',
+  "Let's make your application unforgettable!",
+  'Time to elevate your application!',
+  'Ready to craft some stunning UI?',
+  "Let's bring your vision to life!",
+  'Creating beautiful UI is just a few clicks away!',
+  'Ready to make your application the talk of the town?',
+  'Time to give your application a fresh look!',
+  'Time to wow your next visitor!',
+  'Your design journey starts here!',
+  'Unleash your creativity!',
+  'Accessibility? No problem!',
+  'Bring your UI ideas to life!',
+  'I believe in you! ❤️',
+  'Keep up the great work! ❤️',
+  'Resposiveness? No problem!',
+  'How can I assist you today?',
+]
+
+export const required_config_properties = ['framework', 'output', 'style', 'config']
+export const supported_frameworks = ['react', 'react-native', 'astro', 'solid'] satisfies Frameworks[]
