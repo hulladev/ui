4b8e4d7bcaf19cec322a6e94346e3bdb379d71e6
samuelhulla <oficialny.hulla@gmail.com>
2024-07-29 16:25:06 +0200

    :wrench: update: (ui) style imports

--- packages/ui/src/button/css.ts
+++ packages/ui/src/button/css.ts
@@ -1,4 +1,4 @@
-import { vn } from '@/style'
+import { vn } from '@/lib/style'
 
 export const css = vn({
   base: 'rounded-md px-4 py-2 text-fg',
---
+++ packages/ui/src/card/css.ts
@@ -0,0 +1,3 @@
+import { cn } from '@/lib/style'
+
+export const css = cn('bg-primary-300', 'mx-4')
