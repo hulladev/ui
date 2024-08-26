a39dfdf38480a8b3d11ab38d5f445954c34efabb
samuelhulla <oficialny.hulla@gmail.com>
2024-08-26 15:19:53 +0200

    :white_check_mark: feat: (gen) switch

---
+++ generated/solid/tailwind/switch/Switch.tsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource solid-js **/
+import { cn } from '@/lib/style'
+import type { JSX, ParentProps } from 'solid-js'
+
+export type SwitchProps = ParentProps<JSX.ButtonHTMLAttributes<HTMLButtonElement>> & {
+  checked?: boolean
+}
+
+export function Switch({ children, checked, ...props }: SwitchProps) {
+  return (
+    <button
+      {...props}
+      type="button"
+      aria-checked={!!checked}
+      class={cn(
+        'min-w-xl group relative inline-flex h-[1.25rem] flex-shrink-0 cursor-pointer rounded-full border-2 transition-colors duration-200 ease-in-out',
+  'dark:border-fgdark/80 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-55 disabled:opacity-70',
+        checked ? 'bg-primary hover:bg-primary/80 border-primary/20' : 'bg-fg/70 hover:bg-fg/60 border-fg/20'
+      )}
+    >
+      <span class={cn('bg-bg shadow-shadow dark:bg-fgdark pointer-events-none flex h-[1.4rem] w-[1.4rem] -translate-y-1 transform items-center justify-center rounded-full leading-none shadow-inner ring-0 drop-shadow-lg transition-all duration-200 ease-in-out', checked ? 'translate-x-lg' : '-translate-x-1')}>{children}</span>
+      <span class="sr-only">{checked ? 'On' : 'Off'}</span>
+    </button>
+  )
+}
