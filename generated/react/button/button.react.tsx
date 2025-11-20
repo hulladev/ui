ca5aa2ca6e95589c151dce274c38c9817ba4ee81
samuelhulla <hulla@hulla.dev>
2025-11-20 19:04:18 +0100

    :white_check_mark: feat: apps sandboxes

---
+++ generated/react/button/button.react.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/style"
+import { variant } from "@/style"
+import { VariantProps } from "@hulla/style"
+import type { ButtonHTMLAttributes, DetailedHTMLProps, PropsWithChildren } from "react"
+
+const buttonVariant = variant({
+  name: "variant",
+  classes: {
+    primary: "bg-blue-500",
+    outlined: "bg-transparent border border-gray-300",
+  },
+  base: "cursor-pointer rounded-sm",
+  default: "primary",
+})
+
+export type ButtonProps = PropsWithChildren<
+  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+> &
+  VariantProps<typeof buttonVariant>
+
+export function Button({ children, className, variant, ...props }: ButtonProps) {
+  return (
+    <button {...props} className={cn(buttonVariant.css(variant), className)}>
+      {children}
+    </button>
+  )
+}
