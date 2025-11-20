4969cd5480da3aa0f6bb8e62730e35ff7fc12f93
samuelhulla <hulla@hulla.dev>
2025-11-20 19:00:24 +0100

    :white_check_mark: feat: (app sandboxes)

---
+++ apps/astro/src/styles/index.ts
@@ -0,0 +1,6 @@
+import { style } from "@hulla/style"
+import { twMerge } from "tailwind-merge"
+
+export const { cn, vn } = style({
+  compose: twMerge,
+})
---
+++ apps/solid/src/App.tsx
@@ -0,0 +1,7 @@
+import type { Component } from "solid-js"
+
+const App: Component = () => {
+  return <div class="bg-red-500">solidjs</div>
+}
+
+export default App
